884c58987316936a27597b508c4b4591d572ec33
eevegorov <eevegorov@yandex.ru>
2020-07-06 02:25:41 +0300

    Include ichecks

--- src/app/modules/account/login/login.component.ts
+++ src/app/modules/account/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('ichecks', { static: true }) ichecks: ElementRef;
   error: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    ($(this.ichecks.nativeElement) as any).iCheck({
+      checkboxClass: 'icheckbox_square-green',
+      radioClass: 'iradio_square-green',
+    });
   }
 
 }
