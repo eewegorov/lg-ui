ad4c3fa23bb9bdf661f21ef43941a203eb963574
eevegorov <eevegorov@yandex.ru>
2020-07-18 02:31:07 +0300

    Update Angular 10

--- src/app/core/components/payment-modal/payment-modal.component.ts
+++ src/app/core/components/payment-modal/payment-modal.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentModalComponent implements OnInit {
   public title = '';
-  public planLabels = {};
+  public planLabels;
+  public price;
 
   constructor() { }
 
