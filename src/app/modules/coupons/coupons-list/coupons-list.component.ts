67ca297e18ebbd54910d016f89119e4d21ba85f1
eevegorov <eevegorov@yandex.ru>
2020-08-08 03:01:08 +0300

    Add input prop of coupon-list

--- src/app/modules/coupons/coupons-list/coupons-list.component.ts
+++ src/app/modules/coupons/coupons-list/coupons-list.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-coupons-list',
   templateUrl: './coupons-list.component.html',
   styleUrls: ['./coupons-list.component.scss']
 })
-export class CouponsListComponent implements OnInit {
+export class CouponsListComponent implements OnInit, OnChanges {
   @Input() public coupon;
+  public couponCodeToPaste: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.couponCodeToPaste = `[coupon_${this.coupon.code}]`;
   }
 
 }
--- src/app/modules/coupons/coupons.module.ts
+++ src/app/modules/coupons/coupons.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { CouponsRoutingModule } from './coupons-routing.module';
+import { CouponsComponent } from './coupons/coupons.component';
 import { CouponsListComponent } from './coupons-list/coupons-list.component';
 import { CouponAddComponent } from './coupon-add/coupon-add.component';
 import { CouponMultipleComponent } from './coupon-multiple/coupon-multiple.component';
 import { CouponUniqueComponent } from './coupon-unique/coupon-unique.component';
-import { CouponsComponent } from './coupons/coupons.component';
 
 
 @NgModule({
   declarations: [
+    CouponsComponent,
     CouponsListComponent,
     CouponAddComponent,
     CouponMultipleComponent,
-    CouponUniqueComponent,
-    CouponsComponent
+    CouponUniqueComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     TranslateModule,
     CouponsRoutingModule
   ]
