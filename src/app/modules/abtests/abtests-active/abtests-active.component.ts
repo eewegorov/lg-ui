73fe7840db03dc491769ca2bee7c61506431a42d
Evgeniy Egorov <eevegorov@yandex.ru>
2020-08-09 12:55:23 +0300

    Add ab-tests-active layout

--- src/app/modules/abtests/abtests-active/abtests-active.component.ts
+++ src/app/modules/abtests/abtests-active/abtests-active.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./abtests-active.component.scss']
 })
 export class AbtestsActiveComponent implements OnInit {
+  public currSite;
+  public site = {
+    name: 'dfdsfs'
+  };
+  public test = {
+    id: 'dfsdfds12',
+    name: 'dsfsfsf',
+    description: 'dsfsfdsf'
+  }
+  public item = {
+    name: 'dfdsfds',
+    convInfo: {
+      n: 2,
+      s: 3,
+      conversion: 23
+    }
+  }
+  public type = {
+    type: 'asddad',
+    title: 'dsfdsfds'
+  };
+  public mckp = {
+    preview: 'dfdsfsfs',
+    name: 'dfdsfsf',
+    description: 'dsfsffds'
+  };
+  public grp = {
+    name: 'sdfsf'
+  };
+  public cat = {
+    name: 'dsfsdf'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public getSiteName(site) {
+
+  }
+
+  public getCroppedString(siteName, count, end) {
+
   }
 
 }
--- src/app/modules/abtests/abtests.module.ts
+++ src/app/modules/abtests/abtests.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { UiSwitchModule } from 'ngx-ui-switch';
 import { AbtestsRoutingModule } from './abtests-routing.module';
 import { AbtestsActiveComponent } from './abtests-active/abtests-active.component';
 import { AbtestsArchiveComponent } from './abtests-archive/abtests-archive.component';
@@ -12,6 +14,8 @@ import { AbtestsArchiveComponent } from './abtests-archive/abtests-archive.compo
   ],
   imports: [
     CommonModule,
+    TranslateModule,
+    UiSwitchModule,
     AbtestsRoutingModule
   ]
 })
