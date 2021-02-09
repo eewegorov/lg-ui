b2e8006ae8a8f7f36beba2ea3bd45c4cb3e50a3e
eevegorov <eewegorov@gmail.com>
2021-02-10 02:36:59 +0300

    Add smartpoints locales and implement layout bindings

--- src/app/modules/widgets/smartpoints-control/smartpoints-control.component.ts
+++ src/app/modules/widgets/smartpoints-control/smartpoints-control.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { SmartPoint } from '../../../core/models/widgets';
 import { Smartpoints } from '../../../core/models/sites';
 import { WidgetService } from '../services/widget.service';
 
