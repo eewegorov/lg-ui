de774ffaba4f9629766a6be163a7585dad0e390a
eevegorov <eewegorov@gmail.com>
2021-03-17 00:58:32 +0300

    Fix autoinvite and restrictions template

--- src/app/modules/widgets/widget-edit/widget-edit.component.ts
+++ src/app/modules/widgets/widget-edit/widget-edit.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FlowDirective } from '@flowjs/ngx-flow';
 import { SubscriptionLike } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { FlowDirective } from '@flowjs/ngx-flow';
 
 @Component({
   selector: 'app-widget-edit',
@@ -10,9 +11,20 @@ import { SubscriptionLike } from 'rxjs';
 export class WidgetEditComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('flow') public flow: FlowDirective;
 
+  public weekDays = [];
   private autoUploadSubscription: SubscriptionLike;
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    this.weekDays = [
+      { id: 0, name: this.translate.instant('global.week.monday') },
+      { id: 1, name: this.translate.instant('global.week.tuesday') },
+      { id: 2, name: this.translate.instant('global.week.wednesday') },
+      { id: 3, name: this.translate.instant('global.week.thursday') },
+      { id: 4, name: this.translate.instant('global.week.friday') },
+      { id: 5, name: this.translate.instant('global.week.saturday') },
+      { id: 6, name: this.translate.instant('global.week.sunday') }
+    ];
+  }
 
   ngOnInit(): void {
   }
