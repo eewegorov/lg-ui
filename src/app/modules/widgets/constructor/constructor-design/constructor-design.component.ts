5a6aa067689828302c144e010a3c0c95b27157a6
eevegorov <eewegorov@gmail.com>
2021-03-24 02:26:12 +0300

    Add ladda

--- src/app/modules/widgets/constructor/constructor-design/constructor-design.component.ts
+++ src/app/modules/widgets/constructor/constructor-design/constructor-design.component.ts
@@ -75,6 +75,14 @@ export class ConstructorDesignComponent implements OnInit, AfterViewInit, OnDest
     }
   }
 
+  public saveWidget() {
+    if (this.isMockup) {
+      this.saveMockupItem();
+    } else {
+      this.saveWidgetItem();
+    }
+  }
+
   private switchWidget(widget, newValue) {
     $('[role="tooltip"]').remove();
     if (widget.active === newValue) { return false; }
@@ -87,6 +95,115 @@ export class ConstructorDesignComponent implements OnInit, AfterViewInit, OnDest
       this.widgetService.switch(this.sid, widget.id, newValue).subscribe((response: boolean) => {
         if (!response) { return false; }
         this.widget.active = newValue;
+      });
+    }
+  }
+
+  private saveMockupItem() {
+    let errorsList = this.runValidators();
+    l.start();
+    for (var i = 0; i < $scope.validators.length; i++) {
+      errorsList = errorsList.concat($scope.validators[i].call(this));
+    }
+
+    if (errorsList.length != 0) {
+      toastr["error"]($scope.localization.save.validation.desc, $scope.localization.save.validation.title);
+      l.stop();
+    } else {
+      $scope.savedInstance = {
+        name: $scope.widget.name,
+        guiprops:  $scope.widget.guiprops,
+        autoinvite:  $scope.widget.autoinvite,
+        restrictions:  $scope.widget.restrictions,
+        autoresponder:  $scope.widget.autoresponder,
+        rules:  $scope.widget.rules
+      };
+      $http({
+        method: 'PUT',
+        url: "/api/v1/mockups/"+$scope.wid,
+        data: angular.toJson($scope.savedInstance),
+        headers: {'Content-Type': 'application/json; charset=utf-8'}
+      })
+        .success(function(response) {
+          if (response.rows.length > 0 && response.rows[0].code == 200) {
+            toastr["success"]($scope.localization.save.done.desc, $scope.localization.save.done.title);
+            setTimeout(function(){
+              l.stop();
+            },1000);
+          }
+        }).error(function() {
+        toastr["error"]($scope.localization.save.error.desc, $scope.localization.save.error.title);
+        l.stop();
+        swal($scope.localization.save.error.title, $scope.localization.save.error.desc, "error");
+      });
+    }
+  }
+
+  private saveWidgetItem() {
+    var currentDate = new Date();
+    $scope.widget.guiprops.dhVisual.lastModifiedDate = currentDate;
+    $scope.widget.guiprops.dhVisual.widget_width_all   = $scope.SP_widget.widget_width_all;
+    $scope.widget.guiprops.dhVisual.widget_height_all  = $scope.SP_widget.widget_height_all;
+    $scope.widget.guiprops.dhVisual.widget_width_nopx  = $scope.SP_widget.widget_width_nopx;
+    $scope.widget.guiprops.dhVisual.widget_height_nopx = $scope.SP_widget.widget_height_nopx;
+    $scope.widget.guiprops.dhVisual.widget_ul_width_nopx = $scope.SP_widget.widget_ul_width_nopx;
+    $scope.widget.guiprops.dhVisual.CP_width = $scope.SP_widget.widget_CP_width;
+    $scope.widget.guiprops.dhVisual.CP_offset_top = $scope.SP_widget.widget_CP_offset_top;
+    $scope.widget.guiprops.image.width = $scope.SP_widget.img_width;
+    $scope.widget.guiprops.image.height = $scope.SP_widget.img_height;
+
+    console.log("TWO ", $scope.widget.guiprops.dhVisual.widget_width_all);
+    addCouponsId();
+    mapFormExtFieldId();
+    var errorsList = runValidators();
+    l.start();
+    for (var i = 0; i < $scope.validators.length; i++) {
+      errorsList = errorsList.concat($scope.validators[i].call(this));
+    }
+
+    if (errorsList.length != 0) {
+      toastr["error"]($scope.localization.save.validation.desc, $scope.localization.save.validation.title);
+      l.stop();
+    } else if (couponsErrorFlag) {
+      toastr["error"]($translate.instant("widgets.constructor.coupon.saveError"), $scope.localization.save.validation.title);
+      l.stop();
+    } else if (formExtIdsErrorFlag) {
+      toastr["error"]($translate.instant("widgets.constructor.formExtId.saveError"), $scope.localization.save.validation.title);
+      l.stop();
+    } else if (formExtNeedButton) {
+      toastr["error"]($translate.instant("widgets.constructor.formExtId.actionButton.saveError"), $scope.localization.save.validation.title);
+      l.stop();
+    } else if (formExtRedirectFieldEmpty) {
+      toastr["error"]($translate.instant("widgets.constructor.formExtId.redirectError"), $scope.localization.save.validation.title);
+      l.stop();
+    } else if ($scope.widget.useCustomIntegrationsList && !$scope.widget.integrations.length) {
+      toastr["error"]($translate.instant("widgets.constructor.integration.saveError"), $scope.localization.save.validation.title);
+      l.stop();
+    } else {
+      $scope.widget.jsInfo.onShowScript.script = $scope.widget.jsInfo.onShowScript.script || null;
+      $scope.widget.jsInfo.onTargetScript.script = $scope.widget.jsInfo.onTargetScript.script || null;
+      var widgetUpdatedData = {
+        guiprops: $scope.widget.guiprops,
+        autoinvite: $scope.widget.autoinvite,
+        autoresponder: $scope.widget.autoresponder,
+        restrictions: $scope.widget.restrictions,
+        rules: $scope.widget.rules,
+        audiencesEnabled: $scope.widget.audiencesEnabled,
+        sendCrm: $scope.widget.sendCrm,
+        coupons: $scope.couponsId,
+        integrations: $scope.widget.integrations,
+        useCustomIntegrationsList: $scope.widget.useCustomIntegrationsList,
+        customFields: $scope.customFields,
+        jsInfo: $scope.widget.jsInfo
+      };
+
+      WidgetService.updateWidget($scope.sid, $scope.widget.id, widgetUpdatedData).then(function(response) {
+        if (response && response.success) {
+          toastr["success"]($scope.localization.save.done.desc, $scope.localization.save.done.title);
+        }
+        setTimeout(function(){
+          l.stop();
+        }, 1000);
       });
     }
   }
