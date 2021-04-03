2c77c8f8b8a9983b044fedb6fabc7f4c103e87bf
eevegorov <eewegorov@gmail.com>
2021-04-04 01:52:08 +0300

    Add crm element, form element

--- src/app/modules/widgets/constructor/design/form-element/form-element.component.ts
+++ src/app/modules/widgets/constructor/design/form-element/form-element.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-form-element',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-element.component.scss']
 })
 export class FormElementComponent implements OnInit {
+  public optionsRound: Options = {
+    floor: 0,
+    ceil: 50,
+    step: 1
+  };
+
+  public optionsOpacity: Options = {
+    floor: 0.00,
+    ceil: 1.00,
+    step: 0.1
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public trackById(index, item) {
+    return item.id;
   }
 
 }
