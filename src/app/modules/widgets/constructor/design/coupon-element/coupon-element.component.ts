601986e39a92d0359dfd4b4dea506cb8fbf0202e
eevegorov <eewegorov@gmail.com>
2021-04-04 00:24:06 +0300

    Fix coupon element

--- src/app/modules/widgets/constructor/design/coupon-element/coupon-element.component.ts
+++ src/app/modules/widgets/constructor/design/coupon-element/coupon-element.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-coupon-element',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coupon-element.component.scss']
 })
 export class CouponElementComponent implements OnInit {
+  public optionsRound: Options = {
+    floor: 0,
+    ceil: 50,
+    step: 1
+  };
+
+  public optionsOpacity: Options = {
+    floor: 0.00,
+    ceil: 1.00,
+    step: 0.1
+  };
 
   constructor() { }
 
