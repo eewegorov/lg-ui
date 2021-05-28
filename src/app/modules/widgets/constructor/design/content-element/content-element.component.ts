ea74f389eadd889c3be8d4c372b5ef0f2e2707f8
eevegorov <eewegorov@gmail.com>
2021-05-28 18:01:19 +0300

    Fix widget-image styles

--- src/app/modules/widgets/constructor/design/content-element/content-element.component.ts
+++ src/app/modules/widgets/constructor/design/content-element/content-element.component.ts
@@ -5,7 +5,7 @@ import { WidgetConstructorService } from '../../../services/widget-constructor.s
 @Component({
   selector: 'app-content-element',
   templateUrl: './content-element.component.html',
-  styleUrls: ['./content-element.component.scss']
+  styleUrls: ['../../../shared/shared.scss', './content-element.component.scss']
 })
 export class ContentElementComponent implements OnInit {
   @Input() public widget: FullWidget;
