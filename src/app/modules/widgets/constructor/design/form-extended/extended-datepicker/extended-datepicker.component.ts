75fda72f888f8d7bb8db11405416abb54987e999
eevegorov <eewegorov@gmail.com>
2021-08-06 01:10:05 +0200

    Add extended datepicker

--- src/app/modules/widgets/constructor/design/form-extended/extended-datepicker/extended-datepicker.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-datepicker/extended-datepicker.component.ts
@@ -6,14 +6,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['../../../../shared/shared.scss', './extended-datepicker.component.scss']
 })
 export class ExtendedDatepickerComponent implements OnInit {
-  @Input() public index: number;
   @Input() public item: Record<string, any>;
   @Input() public st: Record<string, any>;
 
+  public isFilled = false;
+
+  public options: object = {
+    autoclose: true,
+    orientation: 'bottom',
+    container: '.max-h-100',
+    locale: 'ru',
+    showClose: false
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    this.options = {
+      ...this.options,
+      format: this.item.dateType.value
+    };
   }
 
 }
