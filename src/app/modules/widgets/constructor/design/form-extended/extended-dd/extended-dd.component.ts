42215eaf317c4e4d97af9c46416e51e92e29ad87
eevegorov <eewegorov@gmail.com>
2021-08-06 23:46:05 +0200

    Fix dd, ddw

--- src/app/modules/widgets/constructor/design/form-extended/extended-dd/extended-dd.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-dd/extended-dd.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { WidgetConstructorService } from '../../../../services/widget-constructor.service';
 
 @Component({
   selector: 'app-extended-dd',
@@ -9,9 +10,38 @@ export class ExtendedDdComponent implements OnInit {
   @Input() public index: number;
   @Input() public item: Record<string, any>;
 
-  constructor() { }
+  public availItemTypes = [];
+  public availWidthTypes = [];
+  public unmappedVariants = [];
+
+  constructor(private widgetConstructorService: WidgetConstructorService) { }
 
   ngOnInit(): void {
+    this.widgetConstructorService.changeArrayOfFormExtTypes.subscribe(() => {
+      this.availItemTypes = this.widgetConstructorService.getAvailableTypes();
+    });
+    this.availWidthTypes = this.widgetConstructorService.getExtFormWidthTypes();
+    this.unmappedVariants = this.mappedCoupons(this.item.variants);
+  }
+
+  public changeFormType(type) {
+    this.widgetConstructorService.changeItemFormType.next({ type, index: this.index });
+  }
+
+  public unmappedValue() {
+    this.item.variants = this.unmappedCoupons(this.unmappedVariants);
+  }
+
+  public idFocusOut() {
+    this.widgetConstructorService.formExtIdFieldFocusOut.next();
+  }
+
+  private unmappedCoupons(list) {
+    return list.split('\n');
+  }
+
+  private mappedCoupons(list) {
+    return list.join('\n');
   }
 
 }
--- src/app/modules/widgets/constructor/design/form-extended/extended-ddw/extended-ddw.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-ddw/extended-ddw.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-extended-ddw',
@@ -9,9 +9,27 @@ export class ExtendedDdwComponent implements OnInit {
   @Input() public item: Record<string, any>;
   @Input() public st: Record<string, any>;
 
+  @ViewChild('element') element: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    $(this.element.nativeElement).bind('click', (event) => {
+      event.stopPropagation();
+    });
+
+    $(document).bind('click', () => {
+      this.item.serviceData.isOpen = false;
+    });
+
+    $('#widgetFormBlockMExt').bind('click', () => {
+      this.item.serviceData.isOpen = false;
+    });
+  }
+
+  public setFormExtDDState(item, variant) {
+    item.serviceData.isOpen = !item.serviceData.isOpen;
+    item.serviceData.extraLabel = variant;
   }
 
 }
