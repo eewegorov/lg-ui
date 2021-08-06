1f8a1427dd73a855e043dc50df0358a48b8de5eb
eevegorov <eewegorov@gmail.com>
2021-08-07 01:22:48 +0200

    Implement extended hidden

--- src/app/modules/widgets/constructor/design/form-extended/extended-hidden/extended-hidden.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-hidden/extended-hidden.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { WidgetConstructorService } from '../../../../services/widget-constructor.service';
 
 @Component({
   selector: 'app-extended-hidden',
@@ -9,9 +10,35 @@ export class ExtendedHiddenComponent implements OnInit {
   @Input() public index: number;
   @Input() public item: Record<string, any>;
 
-  constructor() { }
+  public availItemTypes = [];
+  public fieldTypes = [];
+
+  constructor(private widgetConstructorService: WidgetConstructorService) { }
 
   ngOnInit(): void {
+    this.widgetConstructorService.changeArrayOfFormExtTypes.subscribe(() => {
+      this.availItemTypes = this.widgetConstructorService.getAvailableTypes();
+    });
+    this.fieldTypes = this.widgetConstructorService.getExtFormHiddenFieldType();
+  }
+
+  public changeFormType(type) {
+    this.widgetConstructorService.changeItemFormType.next({ type, index: this.index });
+  }
+
+  public selectFieldType(fieldType) {
+    this.item.idField = 'field_id_' + fieldType.type;
+    this.item.fieldType = fieldType;
+    this.item.service = fieldType.label;
+  }
+
+  public idFocusOut() {
+    this.widgetConstructorService.formExtIdFieldFocusOut.next();
+  }
+
+  public isFieldUtmOrRef(type) {
+    return type === 'utm_source' || type === 'utm_medium' || type === 'utm_campaign' ||
+      type === 'utm_term' || type === 'utm_content' || type === 'referrer';
   }
 
 }
