ff70c354e754d6cb481178c6a388816fab14b416
eevegorov <eewegorov@gmail.com>
2021-08-07 16:53:08 +0200

    Implement extended name element

--- src/app/modules/widgets/constructor/design/form-extended/extended-name/extended-name.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-name/extended-name.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { WidgetConstructorService } from '../../../../services/widget-constructor.service';
 
 @Component({
   selector: 'app-extended-name',
@@ -9,9 +10,20 @@ export class ExtendedNameComponent implements OnInit {
   @Input() public index: number;
   @Input() public item: Record<string, any>;
 
-  constructor() { }
+  public availItemTypes = [];
+  public availWidthTypes = [];
+
+  constructor(private widgetConstructorService: WidgetConstructorService) { }
 
   ngOnInit(): void {
+    this.widgetConstructorService.changeArrayOfFormExtTypes.subscribe(() => {
+      this.availItemTypes = this.widgetConstructorService.getAvailableTypes();
+    });
+    this.availWidthTypes = this.widgetConstructorService.getExtFormWidthTypes();
+  }
+
+  public changeFormType(type) {
+    this.widgetConstructorService.changeItemFormType.next({ type, index: this.index });
   }
 
 }
