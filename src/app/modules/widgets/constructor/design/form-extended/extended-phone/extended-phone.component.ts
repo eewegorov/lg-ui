2b363f6b8595964b894f3828557600c63a978d1c
eevegorov <eewegorov@gmail.com>
2021-08-07 18:50:29 +0200

    Implement extended phone

--- src/app/modules/widgets/constructor/design/form-extended/extended-phone/extended-phone.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-phone/extended-phone.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { WidgetConstructorService } from '../../../../services/widget-constructor.service';
 
 @Component({
   selector: 'app-extended-phone',
@@ -9,9 +10,27 @@ export class ExtendedPhoneComponent implements OnInit {
   @Input() public index: number;
   @Input() public item: Record<string, any>;
 
-  constructor() { }
+  public availItemTypes = [];
+  public availWidthTypes = [];
+
+  constructor(private widgetConstructorService: WidgetConstructorService) { }
 
   ngOnInit(): void {
+    this.widgetConstructorService.changeArrayOfFormExtTypes.subscribe(() => {
+      this.availItemTypes = this.widgetConstructorService.getAvailableTypes();
+    });
+    this.availWidthTypes = this.widgetConstructorService.getExtFormWidthTypes();
+  }
+
+  public changeFormType(type) {
+    this.widgetConstructorService.changeItemFormType.next({ type, index: this.index });
+  }
+
+  public blockedUnusedChars() {
+    const value = this.item.mask.value;
+    const re = /[\\\/\|\[\]\{\}\.,`~@#$%^&?!;_="<>:'a-zA-Zа-яА-Я]/g;
+
+    this.item.mask.value = value.replace(re, '');
   }
 
 }
