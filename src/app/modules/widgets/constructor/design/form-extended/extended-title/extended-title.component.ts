db1b0eece4c3177de00a71da8a1c5e87a097306d
eevegorov <eewegorov@gmail.com>
2021-08-11 00:45:03 +0200

    Implement extended title element

--- src/app/modules/widgets/constructor/design/form-extended/extended-title/extended-title.component.ts
+++ src/app/modules/widgets/constructor/design/form-extended/extended-title/extended-title.component.ts
@@ -1,17 +1,70 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
+import { Options } from '@angular-slider/ngx-slider';
+import { WidgetConstructorService } from '../../../../services/widget-constructor.service';
 
 @Component({
   selector: 'app-extended-title',
   templateUrl: './extended-title.component.html',
   styleUrls: ['../../../../shared/shared.scss', './extended-title.component.scss']
 })
-export class ExtendedTitleComponent implements OnInit {
+export class ExtendedTitleComponent implements OnInit, DoCheck {
   @Input() public index: number;
   @Input() public item: Record<string, any>;
 
-  constructor() { }
+  public availItemTypes = [];
+  public availWidthTypes = [];
+
+  public optionsOpacity: Options = {
+    floor: 0.00,
+    ceil: 1.00,
+    step: 0.1
+  };
+
+  constructor(private widgetConstructorService: WidgetConstructorService) { }
 
   ngOnInit(): void {
+    this.initPicker();
+
+    this.widgetConstructorService.changeArrayOfFormExtTypes.subscribe(() => {
+      this.availItemTypes = this.widgetConstructorService.getAvailableTypes();
+    });
+    this.availWidthTypes = this.widgetConstructorService.getExtFormWidthTypes();
+  }
+
+  ngDoCheck(): void {
+    if (this.item.font.name) {
+      $('#font-picker-ext-form' + this.index).trigger('setFont', this.item.font.name);
+    }
+  }
+
+  public changeFormType(type) {
+    this.widgetConstructorService.changeItemFormType.next({ type, index: this.index });
+  }
+
+  private initPicker() {
+    setTimeout(() => {
+      ($('#font-picker-ext-form' + this.index) as any).fontselect({
+        placeholder: 'Выберите шрифт',
+        placeholderSearch: 'Поиск...',
+        systemFonts: this.widgetConstructorService.getSystemFontListPicker(),
+        googleFonts: this.widgetConstructorService.getGoogleFontListPicker()
+      }).on('change', (change) => {
+        this.setNewFont(change.value, this.item.font.name);
+      });
+
+      $('#font-picker-ext-form' + this.index).trigger('setFont', this.item.font.name);
+    }, 500);
+  }
+
+  private setNewFont(value, data) {
+    let font = value.replace(/\+/g, ' ');
+
+    // Split font into family and weight
+    font = font.split(':');
+    const fontFamily = font[0];
+
+    data.name = fontFamily;
+    data.fontFamily = '\'' + fontFamily + '\'';
   }
 
 }
